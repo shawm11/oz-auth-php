370a90de506921c7901f7d60ada0d4433e790aa2
shawm11 <shawm117@gmail.com>
2022-01-25 11:51:48 -0800

    build(composer): update dependencies

--- src/Server/ForbiddenException.php
+++ src/Server/ForbiddenException.php
@@ -4,6 +4,11 @@ namespace Shawm11\Oz\Server;
 
 class ForbiddenException extends ServerException
 {
+    /**
+     * @param  string $message
+     * @param  int $code
+     * @param  \Exception|null $previous
+     */
     public function __construct($message = '', $code = 403, $previous = null)
     {
         parent::__construct($message, $code, $previous);
